7ddd82f2cd5803991566e5cf1484037ec4145879
Phil-Mobile <formaphil@gmail.com>
2026-06-08 11:50:50 +0200

    Update sw.js
    update cache après logo

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Lylou-Vely Service Worker
 // Change ce numéro à chaque mise à jour du site pour forcer le rechargement
-const CACHE_VERSION = 'lylou-vely-v6';
+const CACHE_VERSION = 'lylou-vely-v7';
 
 const FICHIERS_A_CACHER = [
   '/',
