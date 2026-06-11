bf26a044f03b258b88566026ef2326115f3bd4b6
Phil-Mobile <formaphil@gmail.com>
2026-06-11 21:21:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Lylou-Vely Service Worker
 // Change ce numéro à chaque mise à jour du site pour forcer le rechargement
-const CACHE_VERSION = 'lylou-vely-v23';
+const CACHE_VERSION = 'lylou-vely-v24';
 
 const FICHIERS_A_CACHER = [
   '/',
